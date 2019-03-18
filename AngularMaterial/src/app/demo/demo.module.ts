74e4ae7dd99f4a3a8d809c39194eed5d8d7ef1ed
MolnarArthur <arthur.molnar@usource.ro>
2019-03-18 16:24:47 +0200

    Flexbox demo

--- AngularMaterial/src/app/demo/demo.module.ts
+++ AngularMaterial/src/app/demo/demo.module.ts
@@ -3,19 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { DemoRoutingModule } from './demo-routing.module';
 import { ButtonsComponent } from './buttons/buttons.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatIconModule, MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
+import { FlexboxComponent } from './flexbox/flexbox.component';
 
 @NgModule({
-  declarations: [ButtonsComponent],
+  declarations: [ButtonsComponent, FlexboxComponent],
   imports: [
     CommonModule,
     DemoRoutingModule,
     MatIconModule,
     MatButtonModule,
     MatCheckboxModule,
-    FormsModule
-    
+    FormsModule,
+    FlexLayoutModule
   ]
 })
 export class DemoModule { }
