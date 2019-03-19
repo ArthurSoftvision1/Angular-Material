433f553bce553d51c0642435b39f4e2712d2cde7
MolnarArthur <arthur.molnar@usource.ro>
2019-03-19 12:42:05 +0200

    Routing Users

--- AngularMaterial/src/app/contactmanager/services/user.service.ts
+++ AngularMaterial/src/app/contactmanager/services/user.service.ts
@@ -3,7 +3,7 @@ import { User } from '../models/user';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
-
+import { resolve } from 'q';
 
 @Injectable()
 export class UserService {
@@ -23,6 +23,19 @@ export class UserService {
     return this._users.asObservable();
   }
 
+  addUser(user: User): Promise<User> {
+    return new Promise((resolver, reject) => {
+      user.id = this.dataStore.users.length + 1;
+      this.dataStore.users.push(user);
+      this._users.next(Object.assign({}, this.dataStore).users);
+      resolver(user);
+    });
+  }
+
+  userById(id: number) {
+    return this.dataStore.users.find(x => x.id == id);
+  }
+
   loadAll() {
     const usersUrl = 'https://angular-material-api.azurewebsites.net/users';
 
@@ -34,4 +47,5 @@ export class UserService {
         console.log("Failed to fetch users");
       });
   }
+
 }
