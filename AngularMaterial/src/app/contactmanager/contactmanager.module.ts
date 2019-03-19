f52d350065df0b75b728b3f42ca9555ba1ee616a
MolnarArthur <arthur.molnar@usource.ro>
2019-03-19 11:06:58 +0200

    Fetching data

--- AngularMaterial/src/app/contactmanager/contactmanager.module.ts
+++ AngularMaterial/src/app/contactmanager/contactmanager.module.ts
@@ -1,32 +1,54 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ConatctmanagerAppComponent } from './conatctmanager-app.component';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MaterialModule } from '../shared/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { ContactmanagerAppComponent } from './contactmanager-app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule } from '@angular/forms';
-import { MatIconModule, MatButtonModule, MatCheckboxModule } from '@angular/material';
-import { Routes, RouterModule } from '@angular/router';
-
+import { UserService } from './services/user.service';
+import { HttpClientModule } from '@angular/common/http';
+import { NotesComponent } from './components/notes/notes.component';
+import { NewContactDialogComponent } from './components/new-contact-dialog/new-contact-dialog.component';
 
 const routes: Routes = [
-  { path: '', component: ConatctmanagerAppComponent, children: [
-    { path: '', component: MainContentComponent }
-  ] },
+  {
+    path: '', component: ContactmanagerAppComponent,
+    children: [
+      { path: ':id', component: MainContentComponent },
+      { path: '', component: MainContentComponent }
+    ]
+  },
   { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
-  declarations: [ConatctmanagerAppComponent, ToolbarComponent, MainContentComponent, SidenavComponent],
   imports: [
     CommonModule,
+    HttpClientModule,
+    MaterialModule,
     FlexLayoutModule,
     FormsModule,
-    MatIconModule,
-    MatButtonModule,
-    MatCheckboxModule,
+    ReactiveFormsModule,
     RouterModule.forChild(routes)
+  ],
+  providers: [
+    UserService
+  ],
+  declarations: [
+    ContactmanagerAppComponent, 
+    ToolbarComponent, 
+    MainContentComponent, 
+    SidenavComponent, 
+    NotesComponent, 
+    NewContactDialogComponent
+  ], 
+  entryComponents: [
+    NewContactDialogComponent
   ]
 })
 export class ContactmanagerModule { }
